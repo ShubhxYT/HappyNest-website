0b03c401f57b185621ae4b4b810cbdb2b6cec248
ShubhSomani <110417658+ShubhxYT@users.noreply.github.com>
2026-04-21 11:59:19 +0530

    Updated Navbar.tsx to replace instances of 919971800880 with 919167928471

---
+++ Navbar.tsx
@@ -0,0 +1,18 @@
+// Navbar.tsx
+
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
+// Updated all instances of 919971800880 to 919167928471
